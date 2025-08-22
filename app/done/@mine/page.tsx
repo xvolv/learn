1afeaf4a34bbf1d90c154ae0b8b49530cc537520
salhadin <salassefa@gmail.com>
2025-08-22 07:33:49 +0300

    adding test

---
+++ app/done/@mine/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="border-b-gray-400 w-[300px] h-[600px]">
+      <h1>mine</h1>
+    </div>
+  );
+};
+
+export default page;
