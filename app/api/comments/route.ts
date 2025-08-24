2e76f8ef8aa6c61540ce698db49e3c676a59cae0
salhadin <salassefa@gmail.com>
2025-08-24 23:58:51 +0300

    this is not the end

--- app/api/comments/route.ts
+++ app/api/comments/route.ts
@@ -1,9 +1,17 @@
 import { comments } from "../../../db/data";
-export async function GET() {
-  return new Response(JSON.stringify({ message: comments }), {
+
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const textQuery = url.searchParams.get("text");
+
+  const result = textQuery
+    ? comments.filter(c =>
+        c.text.toLowerCase().includes(textQuery.toLowerCase())
+      )
+    : comments;
+
+  return new Response(JSON.stringify({ message: result }), {
     status: 200,
-    headers: { "Content-Type": "Application/json" },
+    headers: { "Content-Type": "application/json" },
   });
 }
-
-
