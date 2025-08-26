c22a9ac36734b3ee5a1262d7fb87866a08976dfc
salhadin <salassefa@gmail.com>
2025-08-26 09:04:55 +0300

    this is not just a test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import SearchBox from "./components/SearchBox";
-
 export default function Page() {
   return (
     <main className="flex items-center justify-center min-h-screen">
-      <SearchBox />
+      <h1>this is just a page</h1>
     </main>
   );
 }
