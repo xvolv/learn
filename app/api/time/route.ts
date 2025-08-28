4f571b0dd9b114830a2903bdea9547f699444a59
salhadin <salassefa@gmail.com>
2025-08-28 15:34:57 +0300

    removing to start fresh

--- app/api/time/route.ts
+++
@@ -1,7 +0,0 @@
-export const dynamic = "force-static";
-export const revalidate = 10;
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({ time: new Date().toLocaleTimeString() });
-}
--- app/lib/comments/comments.ts
+++
@@ -1,9 +0,0 @@
-export type Comment = {
-  id: number;
-  text: string;
-};
-export const comments: Comment[] = [
-  { id: 1, text: "First comment!" },
-  { id: 2, text: "Next.js makes life easier." },
-  { id: 3, text: "Loving TypeScript support." },
-];
